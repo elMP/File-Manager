9a863c3d6e9acecfeebfecaff8dbf696cb2a3ebc
kneazle <kneazle@yandex.ru>
2022-12-18 18:08:20 +0300

    feat: add greeting at program start

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+import { parseArgs } from './modules/args.js';
 
+const args = parseArgs();
+
+const userName = args['username'] || 'Anonymous';
+console.log(`Welcome to the File Manager, ${userName}`);
