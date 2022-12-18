eb70ba556cb28c983576db5a1a8492658cafec82
kneazle <kneazle@yandex.ru>
2022-12-19 00:38:52 +0300

    feat: add saying goodbye at program end

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
 import { parseArgs } from './modules/args.js';
+
+const readlineInterface = readline.createInterface({ input, output });
 
 const args = parseArgs();
 
 const userName = args['username'] || 'Anonymous';
 console.log(`Welcome to the File Manager, ${userName}`);
+console.log('You are currently in you working directory');
+
+readlineInterface.on('line', (input) => {
+  if (input === '.exit') readlineInterface.close();
+  else console.log('You are currently in you working directory');
+});
+
+readlineInterface.on('close', () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+});
